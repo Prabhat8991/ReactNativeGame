9c7b1816f50ac21a9b0cc6d1215d5aeb44d645e9
Prabhat8991 <singh.prabhat8991@gmail.com>
2023-10-08 16:07:26 +0530

    initial setup

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import {StyleSheet} from 'react-native';
+
+import StartScreen from './screens/StartScreen';
+
+import {React} from 'react-native'
+
+export default function App() {
+  return <StartScreen/>
+}
+
+const styles = StyleSheet.create({
+ 
+});
---
+++ screens/StartScreen.js
@@ -0,0 +1,12 @@
+import {View, TextInput} from 'react-native'
+import PrimaryButton from '../components/PrimaryButton'
+
+function StartScreen() {
+    return (<View>
+        <TextInput/>
+        <PrimaryButton>Confirm</PrimaryButton>
+        <PrimaryButton>Reset</PrimaryButton>
+    </View>);
+}
+
+export default StartScreen
