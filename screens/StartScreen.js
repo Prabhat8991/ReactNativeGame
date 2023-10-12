182efc24bc51c1f92c32463e877e6219b850f2fa
Prabhat8991 <singh.prabhat8991@gmail.com>
2023-10-12 21:57:44 +0530

    added style to buttons and input text

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,12 +1,40 @@
-import {View, TextInput} from 'react-native'
+import {View, TextInput, StyleSheet} from 'react-native'
 import PrimaryButton from '../components/PrimaryButton'
 
 function StartScreen() {
-    return (<View>
-        <TextInput/>
+    return (<View style = {styles.inputContainer}>
+        <TextInput style = {styles.inputField} maxLength={2}/>
         <PrimaryButton>Confirm</PrimaryButton>
         <PrimaryButton>Reset</PrimaryButton>
     </View>);
 }
+
+const styles = StyleSheet.create({
+   inputContainer :  {
+        marginTop: 100,
+        padding: 20,
+        backgroundColor: '#72063c',
+        margin: 20,
+        elevation: 4,
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowRadius: 6,
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.25
+
+    },
+    inputField: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        color: '#ddb52f',
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        marginBottom: 10,
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    }
+})
 
 export default StartScreen
