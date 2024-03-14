22d0dc38293ae514054356d3c721e606dcd1c766
Prabhat8991 <singh.prabhat8991@gmail.com>
2024-03-14 22:37:34 +0530

    updates

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,13 +1,13 @@
 import {Text, View, Pressable, StyleSheet} from 'react-native';
 
-function PrimaryButton({children, onConfirmPress}) {
+function PrimaryButton({abc, onConfirmPress}) {
     
     return (
         <View style = {buttonStyle.outerContainer}>
         <Pressable
          onPress = {onConfirmPress}
          style = {({pressed}) => pressed? [buttonStyle.innerContainer, buttonStyle.pressed]:buttonStyle.innerContainer} android_ripple={{color: '#630323'}}>
-        <Text style = {buttonStyle.buttonTextStyle}>{children}</Text>
+        <Text style = {buttonStyle.buttonTextStyle}>{abc}</Text>
         </Pressable>
         </View>
     );
